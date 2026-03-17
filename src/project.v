2dcca8a3d9bb73146640b0a27904ed2d3e3bf7dc
Johannes Reibold <johannes@reibold.info>
2026-03-17 22:03:54 +0100

    Add state machine

--- src/project.v
+++ src/project.v
@@ -17,11 +17,29 @@ module tt_um_joh1x_prng (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = 8'h00;
+  assign uio_out = 8'h00;
+  assign uio_oe  = 8'hff; // enable all IOs as outputs
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
 
+
+  reg state;
+  localparam Start = 1'b0;
+  localparam Work = 1'b1;
+
+  always @(posedge clk) begin
+    case(state)
+      Start: begin
+        state <= Work;
+      end
+      Work: begin
+      end
+    endcase
+  end
+
+  always @(negedge rst_n) begin
+    state <= Start;
+  end
 endmodule
